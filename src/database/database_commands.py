f43586dc04f7a29725e631cbc0f295bc2e8afaac
hnasser-dev <hnasser.dev@proton.me>
2024-01-02 22:50:17 +0000

    some progress with commands

--- src/database/database_commands.py
+++ src/database/database_commands.py
@@ -5,8 +5,12 @@ from utils.queries import (
     SELECT_SERVER_IN_SERVERS,
     INSERT_SERVER_INTO_SERVERS,
     ADD_USER_TO_SERVER,
-    DEREGISTER_USER_FROM_ALL_SERVERS,
-    MARK_USER_AS_INACTIVE
+    MARK_USER_AS_INACTIVE,
+    ADD_USER_TO_SERVER,
+    REMOVE_USER_FROM_SERVER,
+    REMOVE_USER_FROM_ALL_SERVERS,
+    SELECT_USER_IN_SERVER,
+    SELECT_USER_SERVERS,
 )
 from utils.helpers import DiscordCtx, ErrorLevel
 import sqlite3
@@ -35,7 +39,7 @@ class DatabaseCommands(DatabaseManager):
         if not users:
             return DBErrorHandler(ErrorLevel.WARNING, f"User ({contxt.user_name}) not in the database.")
         try:
-            self.execute_query(DEREGISTER_USER_FROM_ALL_SERVERS, {"user_id": contxt.user_id})
+            self.execute_query(REMOVE_USER_FROM_ALL_SERVERS, {"user_id": contxt.user_id})
             self.execute_query(MARK_USER_AS_INACTIVE, {"id": contxt.user_id})
             self.conn.commit()
         except Exception as e:
@@ -51,27 +55,66 @@ class DatabaseCommands(DatabaseManager):
             self.execute_query(INSERT_SERVER_INTO_SERVERS, server_params)
             self.conn.commit()
         except sqlite3.IntegrityError as e:
-            return DBErrorHandler(ErrorLevel.ERROR, f"Server ({contxt.user_name}) already in the database.", e)
+            return DBErrorHandler(ErrorLevel.WARNING, f"Server ({contxt.user_name}) already in the database.", e)
         except Exception as f:
             return DBErrorHandler(ErrorLevel.ERROR, exception=f)
 
-    def add_user_to_server(self, user_id, server_id):
+    def add_user_to_server(self, contxt: DiscordCtx):
+        params = {
+            "user_id": contxt.user_id,
+            "server_id": contxt.server_id,
+        }
+        try:
+            self.execute_query(ADD_USER_TO_SERVER, params)
+            self.conn.commit()
+        except sqlite3.IntegrityError as e:
+            return DBErrorHandler(ErrorLevel.WARNING, f"User ({contxt.user_name}) already registred in this server.", e)
+        except Exception as f:
+            return DBErrorHandler(ErrorLevel.ERROR, exception=f)
+
+    def remove_user_from_server(self, contxt: DiscordCtx):
+        params = {
+            "user_id": contxt.user_id,
+            "server_id": contxt.server_id,
+        }
+        try:
+            user_results = self.execute_query(SELECT_USER_IN_SERVER, params)
+        except Exception as e:
+            return DBErrorHandler(ErrorLevel.ERROR, exception=e)
+        else:
+            if len(user_results) < 1:
+                return DBErrorHandler(ErrorLevel.WARNING, f"User ({contxt.user_name}) is not registered in this server.")
+        try:
+            self.execute_query(REMOVE_USER_FROM_SERVER, params)
+            self.conn.commit()
+        except Exception as f:
+            return DBErrorHandler(ErrorLevel.ERROR, exception=f)
+
+    def remove_user_from_all_servers(self, contxt: DiscordCtx):
+        params = {
+            "user_id": contxt.user_id,
+        }
+        try:
+            user_results = self.execute_query(SELECT_USER_SERVERS, params)
+        except Exception as e:
+            return DBErrorHandler(ErrorLevel.ERROR, exception=e)
+        else:
+            if len(user_results) < 1:
+                return DBErrorHandler(ErrorLevel.WARNING, f"User ({contxt.user_name}) is not registered in any servers.")
+        try:
+            self.execute_query(REMOVE_USER_FROM_ALL_SERVERS, params)
+            self.conn.commit()
+        except Exception as f:
+            return DBErrorHandler(ErrorLevel.ERROR, exception=f)
+
+    def add_sesh_for_user(self, contxt: DiscordCtx):
         ...
 
-    def remove_user_from_server(self, user_id, server_id):
+    def get_user_visits(self, contxt: DiscordCtx):
         ...
 
-    def remove_user_from_all_servers(self, user_id):
+    def graphify(self, contxt: DiscordCtx):
         ...
 
-    def add_sesh_for_user(self, user_id, day_offset=0):
-        ...
-
-    def get_user_visits(self, user_id, last_n=None):
-        ...
-
-    def graphify(self, data):
-        ...
-
-    def get_data_for_server(self, server_id):
+    def get_data_for_server(self, contxt: DiscordCtx):
         ...
--- src/utils/queries.py
+++ src/utils/queries.py
@@ -30,8 +30,20 @@ INSERT INTO user_servers (user_id, server_id, registered_time)
 VALUES (:user_id, :server_id, :registered_time)
 """
 
+# register user with server
+ADD_USER_TO_SERVER = """\
+INSERT INTO user_servers (user_id, server_id, registered_time)
+VALUES (:user_id, :server_id, :registered_time)
+"""
+
+# deregister user from server
+REMOVE_USER_FROM_SERVER = """\
+DELETE FROM user_servers
+WHERE user_id = :user_id AND server_id = :server_id
+"""
+
 # deregister user from all servers
-DEREGISTER_USER_FROM_ALL_SERVERS = """\
+REMOVE_USER_FROM_ALL_SERVERS = """\
 DELETE FROM user_servers
 WHERE user_id = :user_id    
 """
@@ -41,4 +53,24 @@ MARK_USER_AS_INACTIVE = """\
 UPDATE users
 SET active = 0
 WHERE id = :id    
+"""
+
+# check if a user is registered in a server
+SELECT_USER_IN_SERVER = """\
+SELECT * FROM user_servers WHERE user_id = :user_id AND server_id = :server_id
+"""
+
+# show all the servers that a user is registered in
+SELECT_USER_SERVERS = """\
+SELECT
+users.name AS name,
+users.id AS discord_id,
+servers.name AS server_name,
+servers.id AS server_id
+FROM users
+INNER JOIN user_servers
+ON users.id = user_servers.user_id
+INNER JOIN servers
+on user_servers.server_id = servers.id
+WHERE users.id = :user_id
 """
