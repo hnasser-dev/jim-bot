92d584a8a8891c2f9fdf82f536b29c8cf0680844
hnasser-dev <hnasser.dev@proton.me>
2024-01-02 11:04:55 +0000

    bot and cogs functional

---
+++ src/bot.py
@@ -0,0 +1,53 @@
+from discord import Intents
+from utils.env import (
+    BOT_TOKEN,
+    BOT_TEST_TOKEN,
+    DEBUG
+)
+from discord.ext import commands
+from pathlib import Path
+import os
+
+
+class MyBot(commands.Bot):
+    def __init__(self, command_prefix, description, intents):
+        super().__init__(
+            command_prefix=command_prefix,
+            description=description,
+            intents=intents
+        )
+
+    async def on_ready(self):
+        print(f"Logged in as {self.user}.")
+
+    async def load_cogs(self):
+        cogs_dir = f"{Path(__file__).parent}/cogs"
+        print(cogs_dir)
+        for file in os.listdir(cogs_dir):
+            if file.endswith(".py"):
+                extension_name = file[:-3]
+                print(extension_name)
+                try:
+                    await self.load_extension(f"cogs.{extension_name}")
+                except Exception as e:
+                    print(e)
+                else:
+                    print(f"{extension_name} loaded")
+
+    async def setup_hook(self):
+        """ Runs when the bot first starts up """
+        await self.load_cogs()
+
+
+def main():
+    token = BOT_TOKEN if DEBUG == "False" else BOT_TEST_TOKEN
+    bot = MyBot(
+        command_prefix="jim/",
+        description="Jim Bot",
+        intents=Intents.all()
+    )
+    bot.run(token)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/cogs/commands.py
@@ -0,0 +1,133 @@
+from discord.ext import commands
+from utils.helpers import DiscordCtx, extract_id
+import utils.funcs as funcs
+
+
+class Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def register(self, ctx):
+        """
+        Register to be tracked by the bot
+        Automatically joins the current server (joinserver), too
+        """
+        contxt = DiscordCtx(ctx)
+        funcs.register_user(user_id=contxt.user_id)
+        funcs.add_user_to_server(user_id=contxt.user_id, server_id=contxt.server_id)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def deregister(self, ctx):
+        """
+        Deregister from the bot
+        """
+        contxt = DiscordCtx(ctx)
+        funcs.deregister_user(user_id=contxt.user_id)
+        funcs.remove_user_from_all_servers(user_id=contxt.user_id)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def joinserver(self, ctx):
+        """
+        Associate yourself with the current server
+        """
+        contxt = DiscordCtx(ctx)
+        funcs.add_user_to_server(user_id=contxt.user_id, server_id=contxt.server_id)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def leaveserver(self, ctx):
+        """
+        Disassociate yourself from the current server
+        """
+        contxt = DiscordCtx(ctx)
+        funcs.remove_user_from_server(user_id=contxt.user_id, server_id=contxt.server_id)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def timezone(self, ctx):
+        contxt = DiscordCtx(ctx)
+        timezone = funcs.get_timezone(user_id=contxt.user_id)
+        await contxt.report(f"Your current timezone is {timezone}.")
+
+    @commands.command()
+    async def settimezone(self, timezone, ctx):
+        contxt = DiscordCtx(ctx)
+        new_timezone = funcs.set_timezone(user_id=contxt.user_id, timezone=timezone)
+        await contxt.report(f"Timezone set to {new_timezone}.")
+
+    @commands.command()
+    async def updatename(self, ctx):
+        """
+        Updates a user's name to their current discord user_name
+        """
+        contxt = DiscordCtx(ctx)
+        old_name, new_name = funcs.update_name(user_id=contxt.user_id, new_name=contxt.user_name)
+        if old_name == new_name:
+            await contxt.report(f"Display name already set to {new_name}.")
+        else:
+            await contxt.report(f"Display name updated to {new_name}.")
+
+    @commands.command()
+    async def sesh(self, ctx):
+        contxt = DiscordCtx(ctx)
+        funcs.add_sesh_for_user(user_id=contxt.user_id)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def seshterday(self, ctx):
+        """
+        Alias for 'sesh yesterday'
+        """
+        contxt = DiscordCtx(ctx)
+        funcs.add_sesh_for_user(user_id=contxt.user_id, day_offset=-1)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def graph(self, ctx, other=None):
+        contxt = DiscordCtx(ctx)
+        user_to_lookup = extract_id(other) if other else contxt.user_id
+        visits = funcs.get_user_visits(user_id=user_to_lookup)
+        graph = funcs.graphify(data=visits)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def table(self, ctx):
+        contxt = DiscordCtx(ctx)
+        table_data = funcs.get_data_for_server(server_id=contxt.server_id)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def visits(self, ctx, other=None):
+        contxt = DiscordCtx(ctx)
+        user_to_lookup = extract_id(other) if other else contxt.user_id
+        visits = funcs.get_user_visits(user_id=user_to_lookup)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def lastvisit(self, ctx, other=None):
+        contxt = DiscordCtx(ctx)
+        user_to_lookup = extract_id(other) if other else contxt.user_id
+        last_visit = funcs.get_user_visits(user_id=user_to_lookup, last_n=1)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def last(self, ctx, num_visits, other=None):
+        """
+        Display the last <num_visits> visits for yourself or <other>
+        """
+        contxt = DiscordCtx(ctx)
+        user_to_lookup = extract_id(other) if other else contxt.user_id
+        last_n_visits = funcs.get_user_visits(user_id=user_to_lookup, last_n=num_visits)
+        await contxt.report("placeholder")
+
+    @commands.command()
+    async def helpme(self, ctx):
+        contxt = DiscordCtx(ctx)
+        await contxt.report("placeholder")
+
+
+async def setup(bot):
+    await bot.add_cog(Commands(bot))
