7eab957ed4b4fe71b3a065f2faae0a1204c2ea08
hnasser-dev <hnasser.dev@proton.me>
2024-01-14 03:48:18 +0000

    fixed graphing bug which didn't show most visits

--- src/utils/graphing.py
+++ src/utils/graphing.py
@@ -12,19 +12,20 @@ def graph_data(start_dt_local, curr_date_dt_local, visit_dates_dt_local, user_na
 
     start_dt_local_normalised_np64 = np.datetime64(np.datetime64(start_dt_local), "D")
     curr_date_normalised_np64 = np.datetime64(np.datetime64(curr_date_dt_local), "D")
-    visit_dates_normalised_np64 = [np.datetime64(np.datetime64(dt), "D") for dt in visit_dates_dt_local]
 
-    # generates a list of dates with times set to 0:00:00 (24 hr incremements starting from a normalised start time)
+    visit_dates_normalised_np64 = [np.datetime64(np.datetime64(dt), "D") for dt in visit_dates_dt_local] # for all visit dates
     all_dates = pd.date_range(start=start_dt_local_normalised_np64, end=curr_date_normalised_np64).to_list()
-    # all gym visits
-    visits = pd.Series(1, index=visit_dates_normalised_np64)
+    all_dates_series = pd.Series(0, index=all_dates) # series representing ALL dates, from start to end
 
-    # adding the extra dates in which the user did not attend the gym - assigning these values 0
-    for date in all_dates:
-        if date not in visits.index: # if the date is not in the visits Series
-            visits.at[date] = 0 # add it in and assign its value to 0
+    for visit_date in visit_dates_normalised_np64:
+        if visit_date in all_dates_series.index: # if the user visited on this day
+            all_dates_series.at[visit_date] = 1 # mark the visit as a "1"
 
-    fig, _ = calplot(visits, cmap=colourmap, colorbar=False, dropzero=False, suptitle=f"{user_name}'s Gym Attendance", yearascending=True)  
+    with open(f"graph_{user_name}.csv", "w", encoding="utf-8") as csv_file:
+        csv_file.write(all_dates_series.to_csv())
+
+    fig, _ = calplot(all_dates_series, cmap=colourmap, colorbar=False, dropzero=False, suptitle=f"{user_name}'s Gym Attendance", yearascending=True)
+
     with tempfile.NamedTemporaryFile(suffix='.png', delete=True) as temp_graph_file:
         fig.savefig(temp_graph_file, bbox_inches='tight', pad_inches=0.3)
         with open(temp_graph_file.name, 'rb') as image_file:
